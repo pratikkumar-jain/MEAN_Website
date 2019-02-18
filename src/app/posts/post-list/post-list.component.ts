eb0817b36a8f17de17c5ce8e3799759a00060cc1
Pratik Jain <pjain22@ncsu.edu>
2019-02-18 05:24:53 -0500

    Added post list component

---
+++ src/app/posts/post-list/post-list.component.ts
@@ -0,0 +1,36 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+
+import { Post } from '../post.model';
+import { PostService } from '../post.service'; 
+
+@Component({
+	selector: 'app-post-list',
+	templateUrl: './post-list.component.html', 
+	styleUrls: ['./post-list.component.css']
+})
+
+export class PostListComponent implements OnInit, OnDestroy{
+	// posts = [
+	// 	{title: 'First Post', content: 'This is the first post\'s content'},
+	// 	{title: 'Second Post', content: 'This is the second post\'s content'},
+	// 	{title: 'Third Post', content: 'This is the third post\'s content'}
+	// ];
+
+	posts: Post[] = [];
+	private postSub: Subscription;
+
+	constructor(public postsService: PostService) {}
+
+	ngOnInit(){
+		this.posts = this.postsService.getPosts();
+		this.postSub = this.postsService.getPostUpdatedListener()
+		.subscribe((posts: Post[]) => {
+			this.posts = posts;
+		})
+	}
+	
+	ngOnDestroy() {
+		this.postSub.unsubscribe();
+	}
+}
